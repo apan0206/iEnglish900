359ba9d8202dbb541d8c6f86bdbb89d379094aa4
apan0206 <panda@Pandas-MacBook-Pro.local>
2013-12-11 17:17:57 +0800

    init project

---
+++ iEnglish900/iEnglish900/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  iEnglish900
+//
+//  Created by  Panda on 12/11/13.
+//  Copyright (c) 2013  Panda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ iEnglish900/iEnglish900/phone/PhoneRootViewController.h
@@ -0,0 +1,13 @@
+//
+//  PhoneRootViewController.h
+//  iEnglish900
+//
+//  Created by  Panda on 12/11/13.
+//  Copyright (c) 2013  Panda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PhoneRootViewController : UIViewController
+
+@end
